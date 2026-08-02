c989370a1bf3dcc74ce2fead32afe92a1c476777
gayatrikaparapu2-svg <gayatrikaparapu2@gmail.com>
2026-08-02 22:11:28 +0530

    Add files via upload

---
+++ AcceptInputs.java
@@ -0,0 +1,17 @@
+package javasample;
+
+
+	import java.util.Scanner;
+
+	public class AcceptInputs {
+		public static void main(String[] args) {
+			System.out.println("Enter two numbers to add: "); 
+			
+			Scanner sc = new Scanner(System.in);
+			int x = sc.nextInt(); //typecasting
+			int y = sc.nextInt();
+			
+			System.out.println("Reslut is: " + (x+y));
+		}
+	}
+
